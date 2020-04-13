65eecde0e30164f6e6f18693cc874ca7a6b937da
JJ Merelo <jjmerelo@gmail.com>
2020-04-13 19:54:39 +0200

    Latest experiments

--- code/run.sh
+++ code/run.sh
@@ -2,7 +2,7 @@
 
 for i in {1..15}
 do
-    filename="logs/lo-t4-${i}.json"
+    filename="logs/lo-t6-${i}.json"
     echo Logging to $filename
-    LOG_TIMELINE_JSON_LINES=$filename ./concurrent-ea-leading-ones.p6 --threads=4
+    LOG_TIMELINE_JSON_LINES=$filename ./concurrent-ea-leading-ones.p6 --threads=6
 done
