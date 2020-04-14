48a72864dd4853e0323d867fc9808a269d7a6426
JJ Merelo <jjmerelo@gmail.com>
2020-04-14 08:15:16 +0200

    Logs another run closes #8

--- code/run.sh
+++ code/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-for i in {1..15}
+for j in {2,4,6}
 do
-    filename="logs/lo-t6-${i}.json"
-    echo Logging to $filename
-    LOG_TIMELINE_JSON_LINES=$filename ./concurrent-ea-leading-ones.p6 --threads=6
+   for i in {1..15}
+     do
+        filename="logs/lo-p512-t${j}-${i}.json"
+        echo Logging to $filename
+        LOG_TIMELINE_JSON_LINES=$filename ./concurrent-ea-leading-ones.p6 --total-population=512 --threads=$j
+     done
 done
