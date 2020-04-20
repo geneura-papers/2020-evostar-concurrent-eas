5bfadcdc0ea040e90290749f52a24fbde9c1a729
JJ Merelo <jjmerelo@gmail.com>
2020-04-20 13:11:48 +0200

    Evaluating starting with concurrent generation
    This is really no big deal, since it just starts stuff a few tenths of a second earlier. There's probably no significant difference, but it still makes things a tiny bit faster.

--- plot-lo.R
+++ plot-lo.R
@@ -9,7 +9,7 @@ data$Evaluation.rate <- data$Evaluations/data$Time
 data$Type <- as.factor(data$Type)
 data.t <- as_tibble(data)
 summaries <- data %>% group_by(Type,Threads) %>% summarise(medianE=median(Evaluations,na.rm=TRUE),medianT=median(Time,na.rm=TRUE),medianR=median(Evaluation.rate,na.rm=TRUE))
-ggplot(data,aes(x=Threads,y=Evaluations,color=Type))+geom_tufteboxplot()+theme_tufte()
+ggplot(data,aes(x=Threads,y=Evaluations,color=Type))+geom_tufteboxplot()+theme_tufte()+scale_y_log10()
 ggplot(data,aes(x=Threads,y=Time,color=Type))+geom_tufteboxplot()+theme_tufte()+scale_y_log10()
 ggplot(data,aes(x=Threads,y=Evaluation.rate,color=Type))+geom_tufteboxplot()+theme_tufte()
 data$type.threads <- paste0(data$Type,data$Threads)
